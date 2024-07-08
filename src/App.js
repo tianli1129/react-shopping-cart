8ec966094fc4a503c31a32eda2403abb6a889531
Tina <liutianli2011@gmail.com>
2024-07-08 13:35:58 +0800

    adding the cart component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,81 @@
 import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
-import Filter from './components/Filter';
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
 class App extends React.Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
       products: data.products,
       size: "",
-      sort: ""
-    }
+      sort: "",
+      cartItems: [],
+    };
   }
 
-  sortProducts=(event)=>{
-      const sort = event.target.value
-      this.setState({
-        sort: sort,
-        products: this.state.products.slice().sort((a,b)=>(
-          sort==="lowest"?
-          a.price>b.price?1:-1:
-          sort==="highest"?
-          a.price<b.price?1:-1:
-          a._id<b._id?1:-1
-        )),
-      })
-  }
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+  };
 
-  filterProducts=(event)=>{
-      if(event.target.value===""){
-        this.setState({
-          size: event.target.value,
-          products: data.products
-        })
-      }else{
-        this.setState({
-          size: event.target.value,
-          products: data.products.filter(product=>
-            product.availableSizes.indexOf(event.target.value)>=0
-          )
-        })
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
       }
-  }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
+  };
 
-  render(){
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    this.setState({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    });
+  };
+
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({
+        size: event.target.value,
+        products: data.products,
+      });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+
+  render() {
     return (
       <div className="grid-container">
         <header>
@@ -53,16 +84,23 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-              <Filter count={this.state.products.length}
-                      size={this.state.size}
-                      sort={this.state.sort}
-                      filterProducts={this.filterProducts}
-                      sortProducts={this.sortProducts}
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
               />
-              <Products products={this.state.products} />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
             </div>
             <div className="sidebar">
-              Cart Items
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
             </div>
           </div>
         </main>
